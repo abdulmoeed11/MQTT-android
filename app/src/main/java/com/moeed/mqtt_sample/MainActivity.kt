601641cf515f667fd4242f4ae7ef77bee1c83eb0
Abdul Moeed <a.moeed11@gmail.com>
2023-10-16 23:38:55 +0300

    All errors resolved, mqtt fully working with subscribe and publish as well

--- app/src/main/java/com/moeed/mqtt_sample/MainActivity.kt
+++ app/src/main/java/com/moeed/mqtt_sample/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.moeed.mqtt_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.moeed.mqtt_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mqtt: MqttHandler
+    private var mqtt: MqttHandler? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
-        mqtt = MqttHandler(this@MainActivity)
         initListeners()
     }
 
@@ -24,21 +25,47 @@ class MainActivity : AppCompatActivity() {
             val username = binding.editTextText3.toString()
             val password = binding.editTextText4.toString()
             if (!brokerURL.isEmpty() && !clientId.isEmpty()){
-                mqtt.connect(brokerURL,clientId,username,password)
+                mqtt = MqttHandler(this@MainActivity, brokerURL, clientId)
+                mqtt!!.connect(username,password)
             }else{
                 Toast.makeText(this@MainActivity, "Enter Info", Toast.LENGTH_LONG).show()
             }
         }
         binding.button3.setOnClickListener {
-            if (mqtt.isConnected()){
-                Toast.makeText(this@MainActivity, "Connected", Toast.LENGTH_SHORT).show()
+            if (mqtt != null){
+                if (mqtt!!.isConnected()){
+                    Toast.makeText(this@MainActivity, "Connected", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this@MainActivity, "Not connected", Toast.LENGTH_SHORT).show()
+                }
             }else{
                 Toast.makeText(this@MainActivity, "Not connected", Toast.LENGTH_SHORT).show()
             }
         }
         binding.button2.setOnClickListener {
-                val messageL = binding.editTextText2.text.toString()
-                mqtt.publish("/moeedLocation", messageL)
+            val messageL = binding.editTextText2.text.toString()
+            val topicL = binding.editTextText6.text.toString()
+            Log.d("TOPIC", topicL)
+            if (messageL.isNotEmpty() && topicL.isNotEmpty()){
+                mqtt?.publish(topicL, messageL)
+            }else{
+                Toast.makeText(this@MainActivity, "Enter topic and message", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.button4.setOnClickListener {
+            if(mqtt!=null){
+                mqtt?.disconnect()
+            }else{
+                Toast.makeText(this@MainActivity, "Connect first", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.button6.setOnClickListener {
+            val topicL = binding.editTextText6.text.toString()
+            if (topicL.isNotEmpty()){
+                mqtt?.subscribe(topicL)
+            }else{
+                Toast.makeText(this@MainActivity, "Enter topic", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/moeed/mqtt_sample/MqttHandler.kt
+++ app/src/main/java/com/moeed/mqtt_sample/MqttHandler.kt
@@ -1,9 +1,9 @@
 package com.moeed.mqtt_sample
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
-import org.eclipse.paho.android.service.MqttAndroidClient
+import info.mqtt.android.service.Ack
+import info.mqtt.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.IMqttActionListener
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.IMqttToken
@@ -13,18 +13,27 @@ import org.eclipse.paho.client.mqttv3.MqttException
 import org.eclipse.paho.client.mqttv3.MqttMessage
 
 
-class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallback {
-    private var client: MqttAndroidClient? = null
-    private val context = context
-    fun connect(brokerUrl: String?, clientId: String?, username: String = "moeed", password: String = "Pakistan123") {
-        client = MqttAndroidClient(context, brokerUrl, clientId)
-        client!!.setCallback(this)
+class MqttHandler constructor(contexts: Context, brokerUrl: String, clientId: String): IMqttActionListener, MqttCallback {
+    private var context: Context
+    private var client: MqttAndroidClient
+    init {
+        context = contexts
+        client = MqttAndroidClient(context,brokerUrl,clientId, Ack.AUTO_ACK)
+    }
+
+
+    fun connect(username: String, password: String) {
+        client.setCallback(this)
         val options = MqttConnectOptions()
-//        options.userName = username
-//        options.password = password.toCharArray()
+        if (username.isNotEmpty()){
+            options.userName = username
+        }
+        if (password.isNotEmpty()){
+            options.password = password.toCharArray()
+        }
+        options.mqttVersion = MqttConnectOptions.MQTT_VERSION_3_1_1
         try {
-            // Connect to the broker
-            client?.connect(options, null, this)
+            client.connect(options, null, this)
         } catch (e: MqttException) {
             e.printStackTrace()
         }
@@ -32,7 +41,7 @@ class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallba
 
     fun disconnect() {
         try {
-            client?.disconnect(null, this)
+            client.disconnect(null, this)
         } catch (e: NullPointerException){
             Toast.makeText(context, "Initialize connection first", Toast.LENGTH_SHORT).show()
         } catch (e: MqttException) {
@@ -40,10 +49,10 @@ class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallba
         }
     }
 
-    fun publish(topic: String?, message: String) {
+    fun publish(topic: String, message: String) {
         try {
             val mqttMessage = MqttMessage(message.toByteArray())
-            client?.publish(topic, mqttMessage)
+            client.publish(topic, mqttMessage,null, this)
         } catch (e: NullPointerException){
             e.printStackTrace()
             Toast.makeText(context, "Initialize connection first", Toast.LENGTH_SHORT).show()
@@ -52,9 +61,10 @@ class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallba
         }
     }
 
-    fun subscribe(topic: String?) {
+    fun subscribe(topic: String) {
         try {
-            client?.subscribe(topic,1, null, this)
+//            client.subscribe(topic,1)
+            client.subscribe(topic,1, null, this)
         } catch (e: NullPointerException){
             e.printStackTrace()
             Toast.makeText(context, "Initialize connection first", Toast.LENGTH_SHORT).show()
@@ -65,7 +75,7 @@ class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallba
 
     fun isConnected(): Boolean{
         try {
-            val connection  = client?.isConnected
+            val connection  = client.isConnected
             connection?.let {
                 if (it) return true
             }
@@ -81,13 +91,11 @@ class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallba
     }
 
     override fun onSuccess(asyncActionToken: IMqttToken?) {
-        Log.d("MQTTxx", "Connection Success")
         Toast.makeText(context, "MQTT Connection success", Toast.LENGTH_SHORT).show()
     }
 
     override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
         exception?.printStackTrace()
-        Log.d("MQTTxx", "Connection Failed")
         Toast.makeText(context, "MQTT Connection fails: ${exception.toString()}",
             Toast.LENGTH_SHORT).show()
     }
@@ -99,7 +107,6 @@ class MqttHandler constructor(context: Context): IMqttActionListener, MqttCallba
 
     override fun messageArrived(topic: String?, message: MqttMessage?) {
         val msg = "Receive message: ${message.toString()} from topic: $topic"
-
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 
